c12fff17833cac221c8b5dc5cd52e97c293cf5c1
Percival Li <p5li@uwaterloo.ca>
2025-02-17 18:57:11 -0500

    Rewrite board with loop

--- src/App.js
+++ src/App.js
@@ -60,21 +60,13 @@ export default function Board() {
 
     return (
         <>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-            </div>
+            {[0, 1, 2].map(row => (
+                <div className="board-row">
+                    {[0, 1, 2].map(col => (
+                        <Square value={squares[row * 3 + col]} onSquareClick={() => handleClick(row * 3 + col)}/>
+                    ))}
+                </div>
+            ))}
             <div>
                 {status}
             </div>
@@ -82,5 +74,6 @@ export default function Board() {
                 <button onClick={resetBoard}>reset</button>
             </div>
         </>
-    );
+    )
+        ;
 }
